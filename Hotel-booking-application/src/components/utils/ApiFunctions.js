862857fb5c7cd300901b087c589ed9869df32c48
nitesh2529 <nitesh2529p@gmail.com>
2026-03-28 02:30:38 +0530

    Update API base URL to production serveri

--- Hotel-booking-application/src/components/utils/ApiFunctions.js
+++ Hotel-booking-application/src/components/utils/ApiFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "http://localhost:8080"
+	baseURL: " https://hotel-booking-app-1-0bl0.onrender.com"
 })
 
 export const getHeader = () => {
