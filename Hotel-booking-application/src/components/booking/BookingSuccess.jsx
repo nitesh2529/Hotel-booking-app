14724a3df76a53363598942c89888b24e43c6a24
nitesh2529 <nitesh2529p@gmail.com>
2026-04-01 00:47:20 +0530

    Refactor BookingSuccess component for better loading state

--- Hotel-booking-application/src/components/booking/BookingSuccess.jsx
+++ Hotel-booking-application/src/components/booking/BookingSuccess.jsx
@@ -3,28 +3,34 @@ import { Link, useLocation } from "react-router-dom"
 import Header from "../common/Header"
 
 const BookingSuccess = () => {
-	const location = useLocation()
-	const message = location.state?.message
-	const error = location.state?.error
-	return (
-		<div className="container">
-			<Header title="Booking Success" />
-			<div className="mt-5">
-				{message ? (
-					<div>
-						<h3 className="text-success"> Booking Success!</h3>
-						<p className="text-success">{message}</p>
-					</div>
-				) : (
-					<div>
-						<h3 className="text-danger"> Error Booking Room!</h3>
-						<p className="text-danger">{error}</p>
-						
-					</div>
-				)}
-			</div>
-		</div>
-	)
+  const location = useLocation()
+
+  if (!location.state) {
+    return <p>Loading...</p>
+  }
+
+  const { message, error } = location.state
+
+  return (
+    <div className="container">
+      <Header title="Booking Status" />
+      <div className="mt-5">
+        {message && (
+          <div>
+            <h3 className="text-success">Booking Success!</h3>
+            <p className="text-success">{message}</p>
+          </div>
+        )}
+
+        {error && (
+          <div>
+            <h3 className="text-danger">Error Booking Room!</h3>
+            <p className="text-danger">{error}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default BookingSuccess
