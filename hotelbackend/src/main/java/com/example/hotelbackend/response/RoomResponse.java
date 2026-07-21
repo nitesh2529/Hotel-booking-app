0c7507ec812be52ed78a42abefbded74fecf4bc0
nitesh2529 <nitesh2529p@gmail.com>
2026-07-21 15:01:23 +0530

    Refactor RoomResponse constructor and formatting
    Refactored RoomResponse constructor to use photo URL instead of byte array and improved formatting.

--- hotelbackend/src/main/java/com/example/hotelbackend/response/RoomResponse.java
+++ hotelbackend/src/main/java/com/example/hotelbackend/response/RoomResponse.java
@@ -3,7 +3,6 @@ package com.example.hotelbackend.response;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.data.annotation.Id;
 
 import lombok.Data;
@@ -19,24 +18,24 @@ public class RoomResponse {
     private BigDecimal roomPrice;
     private boolean isBooked;
     private String photo;
-    private List<BookingResponse>bookings;
+    private List<BookingResponse> bookings;
 
-    public RoomResponse(String id, String roomType, BigDecimal roomPrice,BigDecimal roomNo) {
+    public RoomResponse(String id, String roomType, BigDecimal roomPrice, BigDecimal roomNo) {
         this.id = id;
         this.roomType = roomType;
         this.roomPrice = roomPrice;
-        this.roomNo=roomNo;
+        this.roomNo = roomNo;
     }
 
-   public RoomResponse(String id, String roomType, BigDecimal roomPrice,BigDecimal roomNo, boolean isBooked,
-                       byte[] photoBytes , List<BookingResponse> bookings) {
-       this.id = id;
-       this.roomType = roomType;
-       this.roomPrice = roomPrice;
-       this.roomNo = roomNo;
-       this.isBooked = isBooked;
-       this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
-      this.bookings = bookings;
-   }
+    public RoomResponse(String id, String roomType, BigDecimal roomPrice, BigDecimal roomNo, boolean isBooked,
+                         String photoUrl, List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.roomNo = roomNo;
+        this.isBooked = isBooked;
+        this.photo = photoUrl;
+        this.bookings = bookings;
+    }
 
 }
